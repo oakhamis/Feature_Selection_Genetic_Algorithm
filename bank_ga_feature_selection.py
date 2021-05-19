02cf119a508d9c0bb522d5cd9c06060c13037770
Omar Khamis <50887051+oakhamis@users.noreply.github.com>
2021-05-20 00:47:10 +0200

    Add files via upload

--- bank_ga_feature_selection.py
+++ bank_ga_feature_selection.py
@@ -1,31 +1,36 @@
+############################# Bank Feature Selection #########################
+
+
 """
-Bank Feature Selection
+Libraries to be used for this problem
 """
 
 import pandas as pd 
 import numpy as np 
 import sklearn
+from statistics import mean
 
 from lightgbm import *
 from sklearn.model_selection import *
-from sklearn.naive_bayes import *
 from genetic_selection import GeneticSelectionCV
 from sklearn.metrics import *
 from sklearn.model_selection import *
 
 from sklearn.tree import *
-import warnings
+
 from sklearn.utils import *
 import matplotlib.pyplot as plt
 import itertools
 import DataTable
-from statistics import mean
 
+import warnings
 warnings.filterwarnings("ignore")
 
 mcc = make_scorer(matthews_corrcoef)
 
-"""# Data Preprocessing"""
+"""
+Data Preprocessing
+"""
 
 import os
 
@@ -51,6 +56,10 @@ print(allfeats)
 numcols = set(allfeats)
 numcols = list(numcols)
 
+"""
+Decision Tree
+"""
+
 from sklearn.model_selection import train_test_split
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3, random_state=101)
@@ -60,13 +69,16 @@ from sklearn import tree
 clf = tree.DecisionTreeClassifier()
 clf = clf.fit(x_train, y_train)
 
-tree.plot_tree(clf,max_depth=1)
+tree.plot_tree(clf,max_depth=1  )
 
 y_pred = clf.predict(x_test)
 accuracy_score(y_test, y_pred, normalize=False)/x_test.shape[0]
 
 estimator = tree.DecisionTreeClassifier()
 
+"""
+GA Parameters & Configurations
+"""
 
 report = pd.DataFrame()
 nofeats = [] 
@@ -116,43 +128,17 @@ for var_index ,var in enumerate(variations):
     nofeats.append(len(genfeats)) 
     chosen_feats.append(genfeats) 
     cvscore.append(cv_score)
+
+"""
+Storing Results Into Dataframe & CSV file
+"""
     
 report["No of Feats"] = nofeats
 report["Chosen Feats"] = chosen_feats
 report["Scores"] = cvscore
 
-# for i,fitness_values in  enumerate(best_fitness_values):
-#     plt.plot(fitness_values, label='GA variation'+ str(i))
-# plt.legend(loc='upper left', bbox_to_anchor=(1.05, 1))
-# plt.ylim((0.8825,0.895))
-
-max_fitness_value = [max(i) for i in best_fitness_values[0:9]]
-
-avg_fitness_value = [mean(i) for i in best_fitness_values[0:9]]
-
-
-index = np.argsort(max_fitness_value) 
-
-index = index[::-1]
-
-avg_index = index = np.argsort(avg_fitness_value)
-
-avg_index = avg_index[::-1]
-
-
-
-vars_ = ['Pop. Size '+ str(vara[0]) + ' / Crossover rate '+ str(vara[1]) + ' / Mutation rate ' + str(vara[2]) for vara in variations]
-for i in  index:
-    plt.plot(best_fitness_values[i], label=vars_[i])
-plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1))
-plt.ylim((0.883,0.8936))
-plt.rcParams["figure.figsize"] = (7,7)
-fig.subplots_adjust(bottom=0.25)
-
-
 
 report_final = report.iloc[0:270].copy()
-
 report_final["Scores"] = np.round(report_final["Scores"], 6)
 report_final.sort_values(by = "Scores", ascending = False, inplace = True)
 #report.index
@@ -161,15 +147,37 @@ print(ga_feats)
 
 report_final.to_csv("GA_report.csv",index=False)
 
+max_fitness_value = [max(i) for i in best_fitness_values[0:9]]
+
+index = np.argsort(max_fitness_value) 
+
+index = index[::-1]
+
+"""
+GA Variation Plots
+"""
+
+vars_ = ['Pop. Size '+ str(vara[0]) + ' / Crossover rate '+ str(vara[1]) + ' / Mutation rate ' + str(vara[2]) for vara in variations]
+for i in  index:
+    plt.plot(best_fitness_values[i], label=vars_[i])
+plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1))
+plt.ylim((0.883,0.8936))
+plt.rcParams["figure.figsize"] = (6,6)
+
+
+
+"""
+Error Plots
+"""
+
 # Standerd error calculation for each variation:
-       
 First_std_err = np.std(report.iloc[30:60]['Scores']) / np.sqrt(len(report.iloc[30:60]))
 Second_std_err = np.std(report.iloc[150:180]['Scores']) / np.sqrt(len(report.iloc[150:180]))   
 Third_std_err = np.std(report.iloc[240:270]['Scores']) / np.sqrt(len(report.iloc[240:270]))  
    
    
 # Error Plot / Figure 3 in the report.
-plt.figure(figsize=(20,20))
+plt.figure(figsize=(5,5))
 generation = [x for x in range(len(report.iloc[240:270]))]
 fig, ax = plt.subplots()
 ax.errorbar(generation, report.iloc[30:60]['Scores'],yerr = First_std_err, solid_capstyle='projecting', capsize=2,label='Population size:50, Crossover Rate: 0.8, Mutation Rate:0.1')
@@ -178,48 +186,5 @@ ax.errorbar(generation, report.iloc[240:270]['Scores'],yerr = Third_std_err, sol
 plt.xlabel('Generation')
 plt.ylabel('Fitness Value')
 plt.legend(loc='lower center', bbox_to_anchor=(0.5, 1))
-ax.grid(alpha=0.5, linestyle=':')
+ax.grid(alpha=0.5, linestyle=':')   
 plt.show()
-
-
-
-# from evolutionary_search import EvolutionaryAlgorithmSearchCV
-# # Define the hyperparameter configuration space
-# '''
-# rf_params = {
-#     'n_estimators': np.logspace(1,1.8,num = 10 ,base=20,dtype='int'),
-#     'max_depth': np.logspace(1,2,num = 10 ,base=10,dtype='int'),
-#     "max_features":np.logspace(0.2,1,num = 5 ,base=8,dtype='int'),
-#     "min_samples_split":np.logspace(0.4, 1, num=5, base=10, dtype='int'), #[2, 3, 5, 7, 10],
-#     "min_samples_leaf":np.logspace(0.1,1,num = 5 ,base=11,dtype='int'),
-#     "criterion":['gini','entropy']
-# }
-# '''
-# rf_params = {
-#     #"max_features":range(0,13),
-#     'max_depth': range(5,50),
-#     "min_samples_split":range(2,11),
-#     "min_samples_leaf":range(1,11),
-#     #Categorical(name='criterion', categories=['gini','entropy'])#
-#     "criterion":['gini','entropy']
-# }
-# estimator = tree.DecisionTreeClassifier()
-# print(estimator.get_params().keys())
-# # Set the hyperparameters of GA 
-# ga1 = EvolutionaryAlgorithmSearchCV(estimator=clf,
-#                                    params=rf_params,
-#                                    scoring="accuracy",
-#                                    cv=3,
-#                                    verbose=1,
-#                                    population_size=10,
-#                                    gene_mutation_prob=0.10,
-#                                    gene_crossover_prob=0.5,
-#                                    tournament_size=3,
-#                                    generations_number=5,
-#                                    n_jobs=1)
-# ga1.fit(x_train, y_train)
-# print(ga1.best_params_)
-# print("Accuracy:"+ str(ga1.best_score_))
-
-# print(ga1.all_history_[0])
-# #plt.plot(selector.generation_scores_)
